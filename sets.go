38663a19bd19c1e291658d835341732b400c112a
Andres Orozco <ao15@my.fsu.edu>
2019-12-14 20:17:38 -0500

    add sets methods and test shells

--- sets.go
+++ sets.go
@@ -15,3 +15,42 @@ type InterfaceSet map[interface{}]interface{}
 
 // MapSet is imported set from deckarep/golang-set
 type MapSet mapset.Set
+
+// Add element to StructSet
+func (s StructSet) Add(e interface{}) {
+	s[e] = struct{}{}
+}
+
+// Has element in StructSet
+func (s StructSet) Has(e interface{}) bool {
+	if _, ok := s[e]; ok {
+		return true
+	}
+	return false
+}
+
+// Add element to BoolSet
+func (s BoolSet) Add(e interface{}) {
+	s[e] = true
+}
+
+// Has element in BoolSet
+func (s BoolSet) Has(e interface{}) bool {
+	if _, ok := s[e]; ok {
+		return true
+	}
+	return false
+}
+
+// Add element to InterfaceSet
+func (s InterfaceSet) Add(e interface{}) {
+	s[e] = true
+}
+
+// Has element in InterfaceSet
+func (s InterfaceSet) Has(e interface{}) bool {
+	if _, ok := s[e]; ok {
+		return true
+	}
+	return false
+}
--- sets_test.go
+++ sets_test.go
@@ -1,8 +1,26 @@
 package sets
 
-import "testing"
+import (
+	"testing"
+)
 
 // SetsBench runs benchmarks for different set implementations
 func SetsBench(b *testing.B) {
+
+	b.Run("StructSet bench", func(b *testing.B) {
+
+	})
+
+	b.Run("BoolSet bench", func(b *testing.B) {
+
+	})
+
+	b.Run("InterfaceSet bench", func(b *testing.B) {
+
+	})
+
+	b.Run("MapSet bench", func(b *testing.B) {
+
+	})
 
 }
